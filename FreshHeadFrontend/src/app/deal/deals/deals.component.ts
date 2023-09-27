1e126b316fbca505f3d97fe2bae9c77750f7dd96
uDeMann <94697576+uDeMann@users.noreply.github.com>
2023-09-27 14:48:13 +0200

    Created first FE/BE communiation

--- FreshHeadFrontend/src/app/deal/deals/deals.component.ts
+++ FreshHeadFrontend/src/app/deal/deals/deals.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Deal} from "../../model/deal/deal";
+import {DealService} from "../../service/deal.service";
 
 @Component({
   selector: 'app-deals',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./deals.component.css']
 })
 export class DealsComponent {
+  public AllDeals!: Deal[];
+  public ErrorMassage: string = "";
 
+  constructor(private dealService: DealService) {
+    this.dealService.getAllDeals()
+      .subscribe(element => {
+        if (element == null){
+          this.ErrorMassage == "No deals where found";
+        } else {
+          this.AllDeals = element
+          console.log("ahlllasdjfoj", this.AllDeals)
+        }
+      })
+  }
 }
--- FreshHeadFrontend/src/app/model/deal/deal.ts
+++ FreshHeadFrontend/src/app/model/deal/deal.ts
@@ -4,10 +4,10 @@ export class Deal {
   ID: Guid;
   Title: string;
   Description: string;
-  Image: string;
+  Image: string[];
 
 
-  constructor(ID: Guid, Title: string, Description: string, Image: string) {
+  constructor(ID: Guid, Title: string, Description: string, Image: string[]) {
     this.ID = ID;
     this.Title = Title;
     this.Description = Description;
