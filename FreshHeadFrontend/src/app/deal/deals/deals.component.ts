ca803941241ea92e7764f2b3f2dec776ced49954
uDeMann <94697576+uDeMann@users.noreply.github.com>
2023-10-02 09:04:18 +0200

    Added a overview of all the deals, using backend deals

--- FreshHeadFrontend/src/app/deal/deals/deals.component.ts
+++ FreshHeadFrontend/src/app/deal/deals/deals.component.ts
@@ -8,8 +8,9 @@ import {DealService} from "../../service/deal.service";
   styleUrls: ['./deals.component.css']
 })
 export class DealsComponent {
-  public AllDeals!: Deal[];
+  public AllDeals: Deal[] = [];
   public ErrorMassage: string = "";
+  public mynumbers: number[] = [1,2,43,4,5,7,43,23,423,5,25,2]
 
   constructor(private dealService: DealService) {
     this.dealService.getAllDeals()
@@ -18,7 +19,7 @@ export class DealsComponent {
           this.ErrorMassage == "No deals where found";
         } else {
           this.AllDeals = element
-          console.log("ahlllasdjfoj", this.AllDeals)
+          console.log("hallo", this.AllDeals)
         }
       })
   }
