4f185bf726a4b2732193ad43bb3250c1a325fd77
Lizzy <447704@student.fontys.nl>
2023-10-04 11:18:17 +0200

    stash frontend

--- FreshHeadFrontend/src/app/app.module.ts
+++ FreshHeadFrontend/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { JsonPipe } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 import { DealsComponent } from './deal/deals/deals.component';
 import { InfoDealsComponent } from './deal/info-deals/info-deals.component';
 import { CompanyComponent } from './company/company/company.component';
@@ -16,13 +18,14 @@ import { CreateDealComponent } from './deal/create-deal/create-deal.component';
     InfoDealsComponent,
     CompanyComponent,
     NavbarComponent,
-    CreateDealComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [JsonPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- FreshHeadFrontend/src/app/deal/create-deal/create-deal.component.ts
+++ FreshHeadFrontend/src/app/deal/create-deal/create-deal.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
+import { NgbDateStruct, NgbCalendar, NgbDatepickerNavigateEvent, NgbDatepicker } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @Component({
   selector: 'app-create-deal',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-deal.component.css']
 })
 export class CreateDealComponent {
+  model: NgbDateStruct;
+	date: NgbDateStruct;
+  @ViewChild('someViewChildReference') someViewChildVariable!: ElementRef;
+  @ViewChild('dp') dp!: NgbDatepicker;
 
+	constructor(private calendar: NgbCalendar) {
+    this.model = this.calendar.getToday();
+    this.date = {year: this.model.year, month: this.model.month, day: this.model.day}
+  }
+
+	selectToday() {
+		this.model = this.calendar.getToday();
+  }
+
+
+  onDateNavigate(event: NgbDatepickerNavigateEvent) {
+    //this.date = event.next;
+  }
 }
